27436de40683640475e0069d475014dcf0c5729c
Gregory Houldsworth <gregory.houldsworth@gmail.com>
2020-04-18 16:04:55 +0200

    Add word validity check in utils

--- server/games/countdown.js
+++ server/games/countdown.js
@@ -2,15 +2,14 @@ const utils = require('../utils/')
 
 const numLetters = 6
 
-function randLetter(){
-  return String.fromCodePoint(utils.randinc(65, 90))
-}
-
 module.exports = class Countdown{
   constructor(){
-    this.data = [... new Array(numLetters)].map(randLetter)
+    this.data = [... new Array(numLetters)].map(utils.randLetter)
   }
   getData(){
     return { name: "chiffresetdeslettres", data: { letters: this.data } }
+  }
+  scoreWord(word){
+    return Number(utils.validateWord(word))
   }
 }
