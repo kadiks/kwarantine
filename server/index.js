a31f85198f7e9819a5dc9d34654916a83ae02658
Jénaïc Cambré <me@jenaiccambre.com>
2020-04-18 17:15:38 +0200

    implement game randomize selection

--- server/index.js
+++ server/index.js
@@ -1,28 +1,26 @@
+require('dotenv').config();
 // General Imports & Constants
-const apiversion = "0.0.1"
-const express = require('express')
-const app = express()
-const port = 3000
-const utils = require('./utils')
+const apiversion = '0.0.1';
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+const utils = require('./utils');
 // Game Imports
-const Countdown = require('./games/countdown.js')
-const games = [ Countdown ]
+const Games = require('./games');
+const games = Object.keys(Games).map((g) => Games[g]);
 // Domain Constants
-const numRounds = 1
-
+const numRounds = 1;
 
 app.get('/api', (req, res) => {
-  res.json({"name": "kwarantine", "version": apiversion})
-})
-
+  res.json({ name: 'kwarantine', version: apiversion });
+});
 
 app.get('/api/go', (req, res) => {
-  const rounds = [... new Array(numRounds)].map( () => {
-    return new (utils.pick(games))}
-  )
-  const gameData = rounds.map( g => g.getData() )
-  res.json(gameData)
-})
+  const rounds = [...new Array(numRounds)].map(() => {
+    return new (utils.pick(games))();
+  });
+  const gameData = rounds.map((g) => g.getData());
+  res.json(gameData);
+});
 
-
-app.listen(port, () => console.log('listening'))
+app.listen(port, () => console.log(`listening on port: ${port}`));
