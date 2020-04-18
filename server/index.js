45b9242a3063bbfc60b0dc7889f3f23dc58ab8f5
Gregory Houldsworth <gregory.houldsworth@gmail.com>
2020-04-18 20:37:41 +0200

    Start socket.io

--- server/index.js
+++ server/index.js
@@ -1,26 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 // General Imports & Constants
-const apiversion = '0.0.1';
-const express = require('express');
+const apiversion = "0.0.1";
+const express = require("express");
 const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT;
-const utils = require('./utils');
+const utils = require("./utils");
 // Game Imports
-const Games = require('./games');
-const games = Object.keys(Games).map((g) => Games[g]);
+const Games = require("./games");
+const games = Object.keys(Games).map(g => Games[g]);
 // Domain Constants
 const numRounds = 1;
 
-app.get('/api', (req, res) => {
-  res.json({ name: 'kwarantine', version: apiversion });
+app.get("/api", (req, res) => {
+  res.json({ name: "kwarantine", version: apiversion });
 });
 
-app.get('/api/go', (req, res) => {
+app.get("/api/go", (req, res) => {
   const rounds = [...new Array(numRounds)].map(() => {
     return new (utils.pick(games))();
   });
-  const gameData = rounds.map((g) => g.getData());
+  const gameData = rounds.map(g => g.getData());
   res.json(gameData);
 });
 
-app.listen(port, () => console.log(`listening on port: ${port}`));
+http.listen(port, () => console.log(`listening on port: ${port}`));
